7ef7844838028035dfc1ec4df7082fc59ce3248b
Tanya Hohlova <tanya.hohlova10@mail.ru>
2024-07-01 18:17:09 +0300

    homewoerk

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function creatCard(item) {
+    const itemCard = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const imgCard = itemCard.querySelector('.card__image');
+    const deleteButton = itemCard.querySelector('.card__delete-button');
+    imgCard.src = item.link;
+    imgCard.alt = item.name;
+    itemCard.querySelector('.card__title').textContent = item.name;
 
-// @todo: Функция удаления карточки
+// @todo: Функция удаления карточки 
+    deleteButton.addEventListener('click',function (){
+        const deleteCard = deleteButton.closest('.places__item');
+        deleteCard.remove();
+    });
+
+    return itemCard;
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item){
+    addTemplate = creatCard(item);
+    cardList.append(addTemplate);
+});
